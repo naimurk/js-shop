ffefdaf818754ef504a61232440e98cb267806a8
naimurk <naimurk781@gmail.com>
2023-02-17 03:49:46 +0600

    second

--- js/shop.js
+++ js/shop.js
@@ -1,1 +1,34 @@
-console.log('tik ache ')
+let sl = 1;
+document.getElementById('btn-one').addEventListener('click', 
+function(){
+    const productName = document.getElementById('first-text').innerText
+    const productPrice = document.getElementById('first-price').innerText
+    const productQuantity = document.getElementById('first-quantity').innerText
+    
+    const sum = parseInt(productPrice)*parseInt(productQuantity);
+    TableDisplay(productName, productPrice, productQuantity, sum)
+    sl += 1
+    disabledButton('btn-one')
+    
+})
+
+
+
+
+function TableDisplay (Pname, Pprice, Pquantity, Ptotal){
+    const tableContainer = document.getElementById('table-container');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${sl}</td>
+    <td>${Pname}</td>
+    <td>${Pprice}</td>
+    <td>${Pquantity}</td>
+    <td>${Ptotal}</td>
+
+    `;
+    tableContainer.appendChild(tr);
+}
+
+function disabledButton(id) {
+    document.getElementById(id).setAttribute("disabled", true);
+  }
